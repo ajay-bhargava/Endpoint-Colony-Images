554bdfb91db93067dbdfd6dfb4bfaffdba782f1b
Ajay Bhargava <ajay.bhargava@crick.ac.uk>
2020-06-18 14:41:07 +0100

    updated perimeter script.

--- analysis/in-vitro-edu-analysis/src/R-analysis-scripts/in-vitro-edu-stats-caller.R
+++ analysis/in-vitro-edu-analysis/src/R-analysis-scripts/in-vitro-edu-stats-caller.R
@@ -7,10 +7,13 @@ edu.stats.caller <- function(folder.path){
   source('./src/R-analysis-scripts/colony-coordinate-preprocessor.R')
   source('./src/R-analysis-scripts/EdU-Distances-Function.R')
   source('./src/R-analysis-scripts/colony-area-centroid-analysis.R')
+  source('./src/R-analysis-scripts/perimeter-growth-boundary.R')
 
   #colony.coordinate.preprocessor(folder.path)
   df1 <- colony.area.centroid.analysis(folder.path)
   df2 <- edu.distance.boundary(folder.path)
+  df3 <- perimeter.growth.boundary(folder.path)
+  data <- inner_join(df1, df2)
 
-  return (inner_join(df1, df2))
+  return (inner_join(data, df3))
 }
